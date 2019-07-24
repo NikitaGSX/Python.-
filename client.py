d61f992278babc960f9fd3bd9804dd0424e6a266
Дриманович Никита <n.drimanovich@voskhod.ru>
2019-07-24 11:57:56 +0300

    заменил часть кода на одну регулярку

--- client.py
+++ client.py
@@ -3,9 +3,9 @@
 
 import yaml
 import socket
-from argparse import ArgumentParser
+import argparse
 
-parser = ArgumentParser()
+parser = argparse.ArgumentParser(description='Process some integers.') # Изменил строчку кода следуя примерам из документации
 parser.add_argument(
         '-c', '--config', type=str, required=False,
         help='Sets config file path'
@@ -18,10 +18,10 @@ config  = {
         'buffersize': 1024
         }
 
+
 host        = config.get('host')
 port        = config.get('port')
 buffersize  = config.get('buffersize')
-parser      = ArgumentParser()
 
 if args.config:
     with open(args.config) as file:
@@ -31,13 +31,13 @@ if args.config:
 try:
     sock = socket.socket()
     sock.connect((host, port))
-#    print('Client was started')
+    print(f'Client started with { host }:{ port }')
 
     data = input('Enter data: ')
     sock.send(data.encode())
-#   print(f'Client send { data }')
+
     b_response = sock.recv(buffersize)
-    print(f'Server send data: \n\t { b_response.decode() }')
+    print('Server send data: \n\t { b_response.decode() }')
 
 except KeyboardInterrupt:
     print('\n\tClient shutdown!')
--- list.py
+++ list.py
@@ -10,6 +10,7 @@ os_prod_list    = 'Изготовитель системы'
 os_name_list    = 'Название ОС'
 os_code_list    = 'Код продукта'
 os_type_list    = 'Тип системы'
+regex_name      = r':\s+(.+)'
 spisok          = ["./data/info_1.txt", "./data/info_2.txt", "./data/info_3.txt"]
 
 #def get_data(**kwargs):
@@ -24,20 +25,19 @@ def get_data(prod_name=None, os_name=None, code_name=None, system_name=None):
 
     if prod_name is not None:
 #    if kwargs is getattr(prod_name):
-#    if kwargs == prod_name:
-        os_prod_list = re.findall(r'Изготовитель системы:\s+(.+)', prod_name)
+        os_prod_list = re.findall(regex_name, prod_name)
         return os_prod_list
     elif os_name is not None:
 #    if kwargs is getattr(os_name):
-        os_name_list = re.findall(r'Название ОС:\s+(.+)', os_name)
+        os_name_list = re.findall(regex_name, os_name)
         return os_name_list
     elif code_name is not None:
 #    if kwargs is getattr(code_name):
-        os_code_list = re.findall(r'Код продукта:\s+(.+)', code_name)
+        os_code_list = re.findall(regex_name, code_name)
         return os_code_list
     elif system_name is not None:
 #    if kwargs is getattr(system_name):
-        os_type_list = re.findall(r'Тип системы:\s+(.+)', system_name)
+        os_type_list = re.findall(regex_name, system_name)
         return os_type_list
 
 #with open('./data/read.json') as fjson: # Просто чтение из файла
